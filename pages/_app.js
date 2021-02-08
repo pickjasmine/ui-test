c4784d5b8cd671c13979021a25eeb54eae5f974b
Jasmine Francois <jfrancois@leantechniques.com>
2021-02-08 16:33:17 -0600

    Add some styling, use sass and css modules

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <header>
+                <h1>Rode</h1>
+                <Link href={'/'}>Home</Link>
+            </header>
+            <main>
+                <Component { ...pageProps } />
+            </main>
+            <footer>
+                <p>Footer</p>
+            </footer>
+        </>
+    );
 }
 
 export default MyApp
